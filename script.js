603133163f0407d686204d2a9a026c4dd043ec8a
Alexander Andrés Londoño Espejo <alexlondon07@gmail.com>
2021-11-12 17:56:44 -0500

    New Page

---
+++ script.js
@@ -0,0 +1,226 @@
+/**
+ * Resume/CV template created with Bootstrap 5 by @vmoratog and @jdnichollsc
+ */
+const resume = {
+    firstName: 'Alexander Andrés',
+    lastName: 'Londoño Espejo',
+    jobTitle: 'Software Engineering',
+    city: 'Medellin',
+    postalCode: '',
+    country: 'Colombia',
+    phone: '+57 3122195522',
+    email: 'alexlondon07@gmail.com',
+    education: [
+        {
+            school: 'Instituto Tecnológico Metropolitano ITM',
+            degree: 'Bachelor',
+            graduationDate: '2014',
+            description: 'Software Engineering'
+        },
+        {
+            school: 'Instituto Tecnológico Metropolitano ITM',
+            degree: 'Bachelor',
+            graduationDate: '2012',
+            description: 'Information Systems Technologist'
+        }
+    ],
+    links: [
+        {
+            label: 'GitHub',
+            link: 'https://github.com/alexlondon07'
+        },
+        {
+            label: 'LinkedIn',
+            link: 'https://www.linkedin.com/in/alexanderlondono1/'
+        },
+        {
+            label: 'Website',
+            link: 'https://alexlondon07.github.io/'
+        },
+        {
+            label: 'Twitter',
+            link: 'https://twitter.com/alexlondon07'
+        }
+    ],
+    skills: [
+        'Java',
+        'Spring Boot',
+        'Git',
+        'PHP',
+        'JavaScript',
+        'TypeScript',
+        'Docker',
+        'Microservices',
+        'Spring',
+        'NodeJS',
+        'Angular',
+        'Azure',
+        'Devops',
+        'MySQL',
+        'Python Learning',
+        'Flutter Learning',
+        'React Native',
+        'PostgreSQL',
+        'MongoDB',
+        'CakePHP'
+    ],
+    languages: ['English', 'Spanish'],
+    professionalSummary: `Passionate about technology, code and coffee.☕
+  
+  Experienced Software Developer with a demonstrated history of working in the information technology and services industry. Skilled in Typescript, JavaScript, Java, PHP (Programming Language), (NoSQL), PostgreSQL, MySql, SQL Server, .Spring Boot, Spring, Angular and Azure Devops.
+  
+  🔭 I’m currently working on Angular and Spring Boot, Microservices, Docker
+  🌱 I’m currently learning Python, Flutter, AWS...`,
+    employmentHistory: [
+        {
+            jobTitle: 'Java Developer, Ssr Adv',
+            startDate: 'August  2021',
+            endDate: 'Present',
+            employer: 'Globant',
+            city: 'Remote, Medellín/Colombia',
+            achievements: [
+                'Develop solutions with Microservices using Java technologies and Spring Boot, MySQL',
+                'Developed story/task by following TDD/BDD and Pair programming concepts. Providing daily status in scrum meetings with the client.',
+                'Develop software tests with JUnit and Mockito',
+                'Work with agile methodologies (Scrum) and use of JIRA.',
+                'Environment: Java 8+, RESTAPI, Microservices Restful web Services (Spring Boot), Hystrix, Spring Config server, JSON, Log4j, Maven, Git, Junit, Mockito, Tomcat 9, CD (continuous deployment), DevOps with Docker, Spring Cloud, Relational Database, API gateway pattern, Jenkins.'
+
+            ]
+        },
+        {
+            jobTitle: 'Consult',
+            startDate: 'March 2021',
+            endDate: 'August 2021',
+            employer: 'Axity',
+            city: 'Remote, Medellín/Colombia',
+            achievements: [
+                'Project IGA (Identity Governance and Administration): Design, build and configure applications to meet business process and applications requirements',
+                'Technologies: Java, Spring Boot, Spring, Junit, Mockito, Angular, JEST, AWS, Azure DevOps, SQL',
+            ]
+        },
+        {
+            jobTitle: 'Application Development Analyst',
+            startDate: 'October 2019',
+            endDate: 'February 2021',
+            employer: 'Accenture',
+            city: 'Medellín/Colombia',
+            achievements: [
+                'Design, build and configure applications to meet business process and applications  requirements, using frameworks like Angular 7,8,9, Spring Boot and techonologies  like Azure Devops, Aws.',
+            ]
+        },
+        {
+            jobTitle: 'Backlog Developer Analyst',
+            startDate: 'March 2021',
+            endDate: 'October 2021',
+            employer: 'Konecta Colombia',
+            city: 'Medellín/Colombia',
+            achievements: [
+                'Develop software solutions with  Java with Spring framework, PHP with Laravel 5.8, Angular 8+ ',
+                'Integrate information systems using REST FUL web services and REST APIs.',
+                'Design, develop and document the requirements requested by the customer in the aforementioned technologies.'
+            ]
+        },
+        {
+            jobTitle: 'Junior Analyst Developer',
+            startDate: 'March 2017',
+            endDate: 'March 2019',
+            employer: 'Grupo Réditos',
+            city: 'Medellín/Colombia',
+            achievements: [
+                'Develop and support the implementations of client software requirements in order to ensure effective solutions, using Java, JavaScript as programming languages and Oracle Database Manager, also used technologies such as Android Studio for mobile application development..'
+            ]
+        },
+        {
+            jobTitle: 'Freelancer',
+            startDate: 'February  2017',
+            endDate: 'February 2019',
+            employer: 'Freelancer',
+            city: 'Medellín/Colombia',
+            achievements: [
+                'Custom software development with technologies and programming languages such as Java, PHP, JavaScript, Laravel, nodeJS, Angular, Spring Boot'
+            ]
+        },
+        {
+            jobTitle: 'Software Web Developer',
+            startDate: 'December  2016',
+            endDate: 'February 2017',
+            employer: 'Granada.io',
+            city: 'Medellín/Colombia',
+            achievements: [
+                'Develop software using JavaScript as programming language and framework Angular 2, Typescript'
+            ]
+        },
+
+        {
+            jobTitle: 'Back-End Developer',
+            startDate: '',
+            endDate: '',
+            employer: 'Secuencia 24 S.A.S',
+            city: 'Medellín/Colombia',
+            achievements: [
+                'Develop CMS (Content Management System) for the companys clients websites.',
+                'Develop computer software assuming the role of designer and developer of the computational component, in agreement with the other members of a working group, using technologies such as PHP,HTML5, CSS3,Jquery, Ajax,Laravel,Yii,Bootstrap,AngularJS',
+                'Develop the function of analyst and specifier of computer needs and solutions, as well as being ultimately responsible for the development, testing, implementation and user training of the generated systems.',
+                'Evaluate, design, develop and coordinate information technology projects and intervene in the continuous improvement of product quality.'
+            ]
+        },
+    ],
+    courses: [
+        {
+            name: 'Solid',
+            company: 'Acamica',
+            date: 'Aug 2021',
+        },
+
+        {
+            name: 'Rest Systems in Java',
+            company: 'Acamica',
+            date: 'Aug 2021',
+        },
+        {
+            name: 'Quality code and refactoring',
+            company: 'Acamica',
+            date: 'Aug 2021',
+        },
+              {
+            name: 'Secure Development Workshop',
+            company: 'Netec ',
+            date: 'July 2021',
+        },
+              {
+            name: 'Diploma in Multiplatform Mobile Development with React Native',
+            company: 'Cedesistemas',
+            date: 'Aug 2018',
+        },
+              {
+            name: 'Development for Android mobile devices advanced level',
+            company: 'Cedesistemas',
+            date: 'Sep 2017',
+        }
+    ],
+    photo: 'https://pbs.twimg.com/profile_images/1375628115599355906/z2wAwLQ9_400x400.jpg',
+}
+
+const formatResume = (r) => ({
+    ...r,
+    address: [
+        r.country,
+        r.city,
+        r.postalCode
+    ].filter(Boolean).join(', ')
+})
+
+new Vue({
+    el: "#app",
+    data: formatResume(resume)
+});
+
+/**
+ * Wait for animatable-component to be loaded (Only for VanillaJS)
+ **/
+function animatableLoaded() {
+    document.querySelector('body').classList.remove('d-none');
+}
+if (customElements) {
+    customElements.whenDefined('animatable-component').then(animatableLoaded);
+} else animatableLoaded()
